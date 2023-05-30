045e33b7025dad248570b352f25908f6b264d566
dipanjan51 <dipanjandeb1995@gmail.com>
2023-05-30 09:59:23 +0530

    Added banner image, html pages, modified app.py

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Delhi, India',
+    'salary': 'Rs 15,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'NYC, USA',
+    'salary': '$120,000'
+  },
+]
 
 
 @app.route("/")
 def hello():
-  return "Hello"
+  return render_template('home.html', jobs=JOBS, company_name="Tech Gamma")
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
